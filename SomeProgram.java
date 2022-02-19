9dfc24fd91e73b672836244a4202e834f2ba018d
CaspaaCaspaa <98254888+CaspaaCaspaa@users.noreply.github.com>
2022-02-19 14:56:11 +0100

    Update SomeProgram.java

--- SomeProgram.java
+++ SomeProgram.java
@@ -2,6 +2,7 @@ public class SomeProgram {
     public static void main(String[] args) {
       System.out.println("We are learning to use Git."); 
       System.out.println("We will be Git experts, soon."); 
+	System.out.println("Sneaky tricky."); 
    }
 	public void manageRocket() {
     // TODO implement
