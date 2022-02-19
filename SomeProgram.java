c92b4550383f098fb4271c05850f67fa343d36db
CaspaaCaspaa <98254888+CaspaaCaspaa@users.noreply.github.com>
2022-02-19 14:58:49 +0100

    12345

--- SomeProgram.java
+++ SomeProgram.java
@@ -2,7 +2,7 @@ public class SomeProgram {
     public static void main(String[] args) {
       System.out.println("We are learning to use Git."); 
       System.out.println("We will be Git experts, soon."); 
-	System.out.println("Sneaky tricky."); 
+	System.out.println("Sneaky ricky."); 
    }
 	public void manageRocket() {
     // TODO implement
